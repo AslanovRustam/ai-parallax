e3e29b4664355932627672cfe482a40444b6e235
AslanovRustam <aslanovrg@gmail.com>
2025-05-24 01:06:23 +0300

    add favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -39,6 +39,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="/icon.png?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
       <body
         className={`${inter.variable} ${metrophobic.variable} ${grotesk.variable}`}
       >
